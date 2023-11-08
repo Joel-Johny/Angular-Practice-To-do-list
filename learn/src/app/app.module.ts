b2f131e13e0ec6c4dc754f74c4708800d4539996
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2023-11-08 19:19:12 +0530

    initial setup

--- learn/src/app/app.module.ts
+++ learn/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/Header/header/header.component';
+import { FooterComponent } from './Components/Footer/footer/footer.component';
+import { ButtonComponent } from './Components/Button/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
