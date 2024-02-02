36de24ed376c4cd64098df107037848fc39c06b3
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2024-02-02 18:27:06 +0530

    2 way databinding using event or ngModel

--- learn/src/app/app.component.ts
+++ learn/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   listOfnumbers =[11,2,4,52,5]
+  textNumber='Initial Value'
+  onNumberAdd(){
+    console.log("fn called",this.textNumber)
+  }
+
+  handleChange(event : any){
+    console.log("changed")
+    this.textNumber=event.target.value
+  }
 }
--- learn/src/app/app.module.ts
+++ learn/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MyComp2Component } from './Component/my-comp2/my-comp2.component';
 import { AdderLogicComponent } from './Component/adder-logic/adder-logic.component';
@@ -12,7 +12,7 @@ import { AdderLogicComponent } from './Component/adder-logic/adder-logic.compone
     AdderLogicComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
